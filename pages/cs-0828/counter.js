5036d55d499463d8329185fb2f0bda29b8ee14ae
Melody1106 <101308244+Melody1106@users.noreply.github.com>
2023-08-28 16:52:17 +0800

    0828

---
+++ pages/cs-0828/counter.js
@@ -0,0 +1,18 @@
+//導入react中使用狀態勾子
+import { useState } from 'react'
+
+export default function Counter() {
+  //宣告狀態
+  // [獲得值的變數, 設定值的函式] = useState(初始值)
+  const [total, setTotal] = useState(0)
+  return (
+    <h1
+      onClick={() => {
+        setTotal(total + 1)
+      }}
+      role="presentation"
+    >
+      {total}
+    </h1>
+  )
+}
