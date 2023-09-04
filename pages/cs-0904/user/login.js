e21b351e45aa785b635c2cddbeb4fea1f3e09954
Melody1106 <101308244+Melody1106@users.noreply.github.com>
2023-09-04 15:52:06 +0800

    Link元件

--- pages/cs-0904/user/login.js
+++ pages/cs-0904/user/login.js
@@ -1,5 +1,7 @@
 import { useContext } from 'react'
 import { AuthContext } from '@/context/auth'
+//在不同頁面之間保持登陸狀態 link元件
+import Link from 'next/link'
 
 export default function Login() {
   //解構AuthContext中帶的 auth,setAuth
@@ -23,6 +25,11 @@ export default function Login() {
       >
         登入
       </button>
+      <br />
+      {/* 要使用Link元件取代<a>才能保持住資料，讓資料不會在重新整理時跳轉 */}
+      {/* <a href="/cs-0904/user/profile">資料頁</a> */}
+      <br />
+      <Link href="/cs-0904/user/profile">資料頁(Link)</Link>
     </>
   )
 }
--- pages/cs-0904/user/profile.js
+++ pages/cs-0904/user/profile.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useContext } from 'react'
+import { AuthContext } from '@/context/auth'
+//在不同頁面之間保持登陸狀態 link元件
+import Link from 'next/link'
 
 export default function Profile() {
+  //解構auth
+  const { auth } = useContext(AuthContext)
   return (
     <>
       <h1>會員資料頁</h1>
+      <p>目前登入狀態:{auth.isAuth ? '登入中' : '未登入'}</p>
+      <p>會員姓名: {auth.userData.name}</p>
+      <p>會員email{auth.userData.email}</p>
+      <br />
+      <Link href="/cs-0904/user/login">登入頁link</Link>
     </>
   )
 }
