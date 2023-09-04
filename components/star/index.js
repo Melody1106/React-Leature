389bf7edcab6c80c9bcf4daa1511cca5bb1dd5de
Melody1106 <101308244+Melody1106@users.noreply.github.com>
2023-09-04 14:30:28 +0800

    onRatingChange()

--- components/star/index.js
+++ components/star/index.js
@@ -1,7 +1,7 @@
 // {useState} 定義狀態
 import { useEffect, useState } from 'react'
 import styles from './star.module.css'
-export default function Star({ startRating = 0 }) {
+export default function Star({ startRating = 0, onRatingChange = () => {} }) {
   //click時的評分分數
   //1反樣式: 初始值用傳入props 稱為derived(衍生的)state(狀態)
 
@@ -12,7 +12,7 @@ export default function Star({ startRating = 0 }) {
   //.2為解決此derived(衍生的)state(狀態)問題:使用useEffect
   useEffect(() => {
     setRating(startRating)
-  }, [])
+  }, [startRating])
   //相依性陣列，意旨監聽setRating如有更動時，在執行其中代碼一次
   return (
     <>
@@ -29,6 +29,8 @@ export default function Star({ startRating = 0 }) {
                 key={i}
                 onClick={() => {
                   setRating(score)
+
+                  onRatingChange(score)
                 }}
                 onMouseEnter={() => {
                   setHoverRating(score)
--- pages/cs-0904/star-page.js
+++ pages/cs-0904/star-page.js
@@ -2,27 +2,17 @@ import { useState } from 'react'
 import Star from '@/components/star'
 
 export default function StarPage() {
-  const [start, setStart] = useState(3)
+  const [value1, setValue1] = useState(3)
+  const [value2, setValue2] = useState(5)
+  const [value3, setValue3] = useState(1)
 
   return (
     <>
-      <Star startRating={start} />
-      <Star startRating={1} />
-      <Star startRating={5} />
-      <button
-        onClick={() => {
-          setStart(1)
-        }}
-      >
-        set start = 1
-      </button>
-      <button
-        onClick={() => {
-          setStart(5)
-        }}
-      >
-        set start = 5
-      </button>
+      <Star startRating={value1} onRatingChaange={setValue1} />
+      <Star startRating={value2} onRatingChaange={setValue2} />
+      <Star startRating={value3} onRatingChaange={setValue3} />
+      
+     
     </>
   )
 }
