92aeb8e421dcda1304749f91af966616cd9c1a14
Melody1106 <101308244+Melody1106@users.noreply.github.com>
2023-09-06 13:52:38 +0800

    表單狀態

--- components/todo/list/edit-form.js
+++ components/todo/list/edit-form.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export default function EditForm({ text }) {
+export default function EditForm({ id, text, handleUpdateText }) {
   const [inputText, setInputText] = useState(text)
   return (
     <>
@@ -9,6 +9,11 @@ export default function EditForm({ text }) {
         value={inputText}
         onChange={(e) => {
           setInputText(e.target.value)
+        }}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleUpdateText(id, inputText)
+          }
         }}
       />
       <button>褚存</button>
---
+++ pages/cs-0906/html5-form.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+export default function Html5Form() {
+  const [inputText, setInputText] = useState('')
+  const [textareaText, setTextAreaText] = useState('')
+  const foodOptions = ['三明治', '貝果', '佛卡夏']
+  //food的值只會有四者其一 : '三明治', '貝果', '佛卡夏', ''
+  const [food, setFood] = useState('')
+  //初始值為空白字串代表都沒選中
+  return (
+    <>
+      <h1>可控表單元件</h1>
+      <section id="input-text">
+        <h2>問字輸入框(input text)</h2>
+        <input
+          type="text"
+          value={inputText}
+          onChange={(e) => {
+            setInputText(e.target.value)
+          }}
+        />
+      </section>
+      <section id="textarea">
+        <h2>文字輸入區域(textarea)</h2>.
+        <textarea
+          value={textareaText}
+          onChange={(e) => {
+            setTextAreaText(e.target.value)
+          }}
+        />
+      </section>
+      <section id="radio-group">
+        <h2>選項按鈕群組(radio group)</h2>
+        {foodOptions.map((v, i) => {
+          return (
+            <label key={i}>
+              <input
+                type="radio"
+                //用值和目前選中的狀態來比較，決定是否呈現選中的樣子
+                checked={food === v}
+                value={v}
+                onChange={(e) => {
+                  setFood(e.target.value)
+                  //狀態紀錄的是每個選項傳送的值
+                }}
+              />
+              {v}
+            </label>
+          )
+        })}
+      </section>
+    </>
+  )
+}
